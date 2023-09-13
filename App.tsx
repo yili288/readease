cec8a27175acd33eae10f7d985f6ef9c8901a946
Yi Li Ng <yiling@MacBook-Air.local>
2023-09-12 22:01:05 -0400

    fix: hello world render

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
 import Navigator from './src/navigation/Navigator';
 
 const App = () => {
   return (
+    <NavigationContainer>
       <Navigator></Navigator>
+    </NavigationContainer>
   );
 }
 
