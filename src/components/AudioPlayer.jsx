8fe696d7e590dee883b64b7cc33eee64c02ad5d1
Yi Li Ng <47297703+yili288@users.noreply.github.com>
2023-10-08 13:34:15 -0400

    feat: save audio file in phone and allow audio playback (#2)
    Co-authored-by: Yi Li Ng <yiling@MacBook-Air-4.local>

---
+++ src/components/AudioPlayer.jsx
@@ -0,0 +1,48 @@
+import Sound from 'react-native-sound';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet } from 'react-native';
+import RNFS from 'react-native-fs';
+
+var styles = StyleSheet.create({
+  button: {
+    color: 'blue',
+  },
+})
+Sound.setCategory('Playback');
+
+// fetch audio file
+var audio = new Sound('hello.wav', RNFS.DocumentDirectoryPath, (error) => {
+  if (error) {
+    console.log('failed to load the sound', error.message);
+    return;
+  }
+  // loaded successfully
+});
+
+export const AudioPlayer = () => {
+  // local variable within this component
+  const [play, setPlay] = useState(false);  //initialised to false
+  
+  // Anything written inside useEffect will only run once
+  // at the initial rendering stage
+  useEffect(() => {
+    audio.setVolume(10);
+  })
+
+  return (
+    <Button
+      title={
+        // ternary operator
+        // condition ? if true : if false
+        (play) ? 'Pause' : 'Play'   
+      }
+      onPress={() => {
+        (play) ? audio.stop() : audio.play();
+        setPlay(!play);
+      }}
+      style={styles.button}
+    />
+  );
+};
+
+export default AudioPlayer;
---
+++ src/utils/saveAudioFile.js
@@ -0,0 +1,17 @@
+import RNFS from 'react-native-fs';
+
+const saveAudioFile = (response) => {
+  var path = RNFS.DocumentDirectoryPath + '/hello.wav';
+
+  // decode base 64 and save as audio file
+  RNFS.writeFile(path, response.audioContent, 'base64') 
+    .then((success) => {
+      console.log('FILE WRITTEN!');
+    })
+    .catch((err) => {
+      console.error('Error saving file:', err.message);
+    });
+
+}
+
+export default saveAudioFile
