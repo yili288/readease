2747246313e0e4900447b36ded95aafe28d9ee49
Lucas Choi <77515168+lucasnchoi@users.noreply.github.com>
2023-10-30 00:27:08 -0400

    Feature/main text screen (#7)

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,10 @@
 import React, { useState} from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import { Text, View, Button, TouchableOpacity, Image, StyleSheet, ScrollView } from 'react-native';
 import saveAudioFile from '../utils/saveAudioFile';
 import AudioPlayer from '../components/AudioPlayer';
 import { textToSpeech } from '../utils/textToSpeech';
 
-const Home = (): JSX.Element => {
+const Home = ({ navigation }): JSX.Element => {
   
   const textToAudio = async () => {
     // send file text to server
@@ -20,46 +20,134 @@ const Home = (): JSX.Element => {
     //const url = ""
     //const textJson = loadParseJson(url);
     //hardcoded json file for now until can use fetch to get texts from server
-    const textJson = {'title': "Text Title", 'content': "This is a paragraph of text, I'm making this longer to see what happens when it reaches the edge of the phone screen, spoiler it wraps. \n\nThis is a second paragraph.", 'audio_file_id':"1"}
+    const textJson = {'title': "Neoclassicism and Early Romanticism In Britain", 'content': "British tourists and artists in Italy were the leading supporters of early Neoclassiscism, partly because of the burgeoning taste for revival styles at home. Nonetheless, the British interest in Classical revival styles was inflected slightly differently from Roman Neoclassiscism. While Roman Neoclassiscism looked to the past in order to revive a sense of moral and civic virtue, many later eighteenth-century British artists harnessed the concept of civic virtue to patriotism to create more Romantic works of art dedicated specifically to the British. In conclusion, Neoclassicism in Britain was a significant artistic and architectural movement that celebrated the timeless beauty of classical antiquity, contributed to the Enlightenment's intellectual climate, and left an enduring mark on British culture and aesthetics. It remains an important chapter in the history of British art, architecture, and literature.", 'audio_file_id':"1"}
     setTitle(textJson.title);
     setContent(textJson.content);
   }
 
   return (
-    <View> 
-      <Text style={styles.titleText}>{title}</Text>
-      <Text style={styles.baseText}>{content}{'\n'}</Text>
-      <Button
-        title= 'Display Text'
-        onPress={displayText}
-       />
-      <Button
-        title='Get audio'
-        onPress={textToAudio}
-      />
-      <AudioPlayer/>
+    <View style={styles.homeBackground}>
+      <ScrollView>
+        <View style={styles.homeContainer}> 
+          <View style={styles.topBarContainer}>
+            <TouchableOpacity style={styles.topBarExitButtonContainer} onPress={() => {}}>
+                <Image
+                  style={styles.topBarExitButton}
+                  source={require('../assets/exit.png')}
+                />
+              </TouchableOpacity>
+              <View></View>
+              <TouchableOpacity style={styles.topBarExitButtonContainer} onPress={() => {}}>
+                <Text style={styles.saveButtonText}>Save</Text>
+              </TouchableOpacity>
+          </View>
+          <Text style={styles.titleText}>{title}</Text>
+          <Text style={styles.baseText}>{content}{'\n'}</Text>
+        </View>
+      </ScrollView>
+      <View style={styles.navBarContainer}> 
+            <TouchableOpacity style={styles.navBarButtonContainer} onPress={displayText}>
+              <Image
+                style={styles.navBarButtonImage}
+                source={require('../assets/text.png')}
+              />
+              <Text style={styles.navBarButtonText}>Original</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navBarButtonContainer} onPress={() => navigation.navigate('AudioScreen')}>
+              <Image
+                style={styles.navBarButtonImage}
+                source={require('../assets/headphones.png')}
+              />
+              <Text style={styles.navBarButtonText}>Audio Only</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navBarButtonContainer} onPress={() => {}}>
+              <Image
+                style={styles.navBarButtonImage}
+                source={require('../assets/summary.png')}
+              />
+              <Text style={styles.navBarButtonText}>Summary</Text>
+            </TouchableOpacity>
+          </View>
     </View>
   );
 };
 
-//not sure where I should put this will leave here for now 
 var styles = StyleSheet.create({
-  titleText: {
-    fontSize: 35,
-    fontWeight: 'bold',
-    textAlign: 'center',
+  homeBackground: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  homeContainer: {
+    flex: 1,
+    width: '90%',
+    alignSelf: 'center',
+  },
+  topBarContainer: {
+    position: 'relative',
+    top: 0,
+    left: 0,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 5,
+  },
+  topBarExitButtonContainer: {
+    marginTop: 10,
+  },
+  topBarExitButton: {
+    width: 25,
+    height: 25,
+    alignSelf: 'center',
+  },
+  saveButtonText:{
     color: 'black',
-    paddingLeft:10,
-    paddingRight: 10,
+    fontFamily: 'Lexend Black',
+    fontSize: 20,
+  },
+  titleText: {
+    position: 'relative',
+    fontSize: 30,
+    textAlign: 'left',
+    color: 'black',
+    paddingVertical: 10,
+    lineHeight: 35,
+    fontFamily: 'Lexend Bold',
   },
   baseText: {
-    fontSize: 12,
+    fontSize: 20,
     color: 'black',
-    paddingLeft:5,
-    paddingRight:5,
-    //to set font would need fonts in assets folder
-    //fontFamily: 'Arial',
+    paddingVertical: 10,
+    lineHeight: 35,
+    fontFamily: 'Lexend Black',
   },
+  navBarContainer: {
+    position: 'absolute',
+    bottom: 30,
+    borderRadius: 10,
+    opacity: 0.7,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: '90%',
+    alignSelf: 'center',
+  },
+  navBarButtonContainer: {
+    flex: 1,
+    opacity: 1,
+    alignItems: 'center',
+  },
+  navBarButtonText: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontFamily: 'Lexend Bold',
+    fontSize: 10,
+  },
+  navBarButtonImage:{
+    width: 40,
+    height: 40,
+  }
 });
 
 export default Home;
