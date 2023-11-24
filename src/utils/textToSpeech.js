4bd1339216b6fc8ca86ea3e35da27acb02a29b96
Yi Li Ng <yiling@MacBook-Air-4.local>
2023-11-23 23:30:30 -0500

    working call to server for text to speech, text from Home screen

--- src/utils/textToSpeech.js
+++ src/utils/textToSpeech.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
 
 
-const textToSpeech = async (text) => {
-    const content = {text: text};
+const textToSpeech = async (textId, text) => {
     try{
-        const response = await axios.post('http://localhost:3000/texts/text-to-speech', content)
+        const response = await axios.post('http://localhost:3000/texts/text-to-speech', {text})
         console.log('Text to speech success')
         return response.data
 
     }
     catch(error){
-        console.error('There was an error.', error);
+        console.error('Error converting text to speech.', error);
         return null;
     }
    
