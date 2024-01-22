29f5b0005d90a244bd62583053831b9769739ec8
ZJC <garfield@GarfielddeMacBook-Pro.local>
2024-01-22 12:43:10 -0500

    Fixed the mis-commit of file getTextTitleAndContent.js

--- src/utils/getTextTitleAndContent.js
+++ src/utils/getTextTitleAndContent.js
@@ -1,14 +1,17 @@
 import axios from 'axios'
 const apiUrl = 'http://localhost:3000/texts'
 
-catch(error){
-    console.error('There was an error.', error);
-    return null;
+const getTextTitle = async (textId) => {
+    try{
+      const response = await axios.get(`http://localhost:3000/texts/${textId}`)
+      console.log('Fetched Text')
+      return response.data.foundText
+  
+  }
+    catch(error){
+        console.error('There was an error.', error);
+        return null;
+    }
 }
-}
-
-
-
-
 
 export default getTextTitle
