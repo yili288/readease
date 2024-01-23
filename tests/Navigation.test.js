a61557066a15b63c0b2c0bbd6c452618b24d9bde
Yi Li Ng <yiling@macbook-air-4.home>
2024-01-22 22:26:44 -0500

    audio and summary screen navigation test

--- tests/Navigation.test.js
+++ tests/Navigation.test.js
@@ -1,15 +1,17 @@
 import { render, fireEvent, screen } from '@testing-library/react-native';
 import Home from '../src/screens/Home';
+import Navigator from '../src/navigation/Navigator';
 
 it('Should navigate to audio page', () => {
-  const { getByTestId, getByText } = render(<Home />);
+  render(<Navigator initialRouteName="Home" />);
+  const { getByTestId } = render(<AudioScreen />);
   fireEvent.press(getByTestId('audioButton'));
   expect(screen.name).toBe('AudioScreen');
 });
 
 it('Should navigate to summary page', () => {
-  const { getByTestId, getByText } = render(<Home />);
+  render(<Navigator initialRouteName="Home" />);
+  const { getByTestId } = render(<Home />);
   fireEvent.press(getByTestId('summaryButton'));
   expect(screen.getByText('Points')).toBeOnTheScreen();
 })
-
